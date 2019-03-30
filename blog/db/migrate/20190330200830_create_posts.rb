0adaaa0b45e18eca5a160e9aa3038587b46ebadc
Liam Emmart <l.emmart@outlook.com>
2019-03-30 16:11:21 -0400

    starter files - post created

---
+++ blog/db/migrate/20190330200830_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :caption
+      t.integer :rating
+      t.string :location
+      t.string :time
+      t.text :tags
+
+      t.timestamps
+    end
+  end
+end
