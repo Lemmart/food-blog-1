dc8016fba9c86f7ed0e7baf75db5e1da221627ac
Liam Emmart <l.emmart@outlook.com>
2019-04-09 18:34:15 -0400

    Lauren + Liam: devise added; user created; github login fails due to not passing client_id; local login succeeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   # resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # match '/auth/:provider/callback', :to => 'sessions#create'
+  # match '/auth/failure', :to => 'sessions#failure'
   resources :posts do
     resources :comments
   end
