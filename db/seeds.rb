868092228784784ba7b1ae552366dbf7df361cc5
Lauren Henske <lhenske@colgate.edu>
2019-04-18 15:45:33 -0400

    Issue with comments display resolved. Fix issued in index view, show view, and seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,52 +9,52 @@ Comment.delete_all
 Post.delete_all
 User.delete_all
 # users that create a post
-user1 = User.create!(email: "arewein@example.com", password: "arewein", username: "arewein")
+user1 = User.create!(email: "bill@example.com", password: "arewein", username: "bill")
 user1.save!(validate: true)
 
-user2 = User.create!(email: "arewein@test.com", password: "areare", username: "are")
+user2 = User.create!(email: "bob@example.com", password: "areare", username: "bob")
 user2.save!(validate: true)
 
-user3 = User.create!(email: "test@example.com", password: "testtest", username: "test")
+user3 = User.create!(email: "riley@example.com", password: "testtest", username: "riley")
 user3.save!(validate: true)
 
-user4 = User.create!(email: "example@example.com", password: "example", username: "example")
+user4 = User.create!(email: "sasha@example.com", password: "example", username: "sasha")
 user4.save!(validate: true)
 
-user5 = User.create!(email: "examplen@test.com", password: "examplen", username: "examplen")
+user5 = User.create!(email: "claire@example.com", password: "examplen", username: "claire")
 user5.save!(validate: true)
 
 # users that comment on a post
 
-comment1 = User.create!(email: "arein@example.com", password: "arewein", username: "one")
+comment1 = User.create!(email: "sarah@example.com", password: "arewein", username: "sarah")
 comment1.save!(validate: true)
 
-comment2 = User.create!(email: "wein@test.com", password: "areare", username: "two")
+comment2 = User.create!(email: "molly@test.com", password: "areare", username: "molly")
 comment2.save!(validate: true)
 
-comment3 = User.create!(email: "t@example.com", password: "testtest", username: "three")
+comment3 = User.create!(email: "charlie@example.com", password: "testtest", username: "charlie")
 comment3.save!(validate: true)
 
-comment4 = User.create!(email: "le@example.com", password: "example", username: "four")
+comment4 = User.create!(email: "alex@example.com", password: "example", username: "alex")
 comment4.save!(validate: true)
 
-comment5 = User.create!(email: "plen@test.com", password: "examplen", username: "five")
+comment5 = User.create!(email: "olivia@test.com", password: "examplen", username: "olivia")
 comment5.save!(validate: true)
 
 a = Post.create!(user_id: "#{user1.id}", caption: "Bagel", rating: "5", location: "Frank", time: "10:00pm", tags:"#GoodEATS")
-a.comments.create!(user_id: "#{user1.id}", username: "#{comment1.username}", body: "best bagel ever")
-a.comments.create!(user_id: "#{user1.id}", username: "#{comment2.username}", body: "why is there a hole in the middle? isnt that a donut?")
+a.comments.create!(user_id: "#{comment1.id}", username: "#{comment1.username}", body: "best bagel ever")
+a.comments.create!(user_id: "#{comment2.id}", username: "#{comment2.username}", body: "why is there a hole in the middle? isnt that a donut?")
 
 b = Post.create!(user_id: "#{user2.id}", caption: "Toast", rating: "1", location: "Frank", time: "10:00am", tags:"#burnt")
-b.comments.create!(user_id: "#{user2.id}", username: "#{comment3.username}", body: "how do you mess up toast?")
-b.comments.create!(user_id: "#{user2.id}", username: "#{comment4.username}", body: "loved the toast")
+b.comments.create!(user_id: "#{comment3.id}", username: "#{comment3.username}", body: "how do you mess up toast?")
+b.comments.create!(user_id: "#{comment4.id}", username: "#{comment4.username}", body: "loved the toast")
 
 c = Post.create!(user_id: "#{user3.id}", caption: "Eggs", rating: "3", location: "Coop", time: "12:00pm", tags:"#raw")
-c.comments.create!(user_id: "#{user3.id}", username: "#{comment5.username}", body: "no salt :(")
+c.comments.create!(user_id: "#{comment5.id}", username: "#{comment5.username}", body: "no salt :(")
 
 d = Post.create!(user_id: "#{user4.id}", caption: "Bacon", rating: "4", location: "Coop", time: "8:00am", tags:"#YUMMY")
-d.comments.create!(user_id: "#{user4.id}", username: "#{comment1.username}", body: "bacons good, hard to mess up")
+d.comments.create!(user_id: "#{comment1.id}", username: "#{comment1.username}", body: "bacons good, hard to mess up")
 
 e = Post.create!(user_id: "#{user5.id}", caption: "Cereal", rating: "5", location: "Frank", time: "3:00am", tags:"#bestfoodever")
-e.comments.create!(user_id: "#{user5.id}", username: "#{comment2.username}", body: "I live for the cereal!")
-e.comments.create!(user_id: "#{user5.id}", username: "#{comment3.username}", body: "theres only cereal")
+e.comments.create!(user_id: "#{comment2.id}", username: "#{comment2.username}", body: "I live for the cereal!")
+e.comments.create!(user_id: "#{comment3.id}", username: "#{comment3.username}", body: "theres only cereal")
