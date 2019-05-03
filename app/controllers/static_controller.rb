09841030f5337b6f7e23d1ea74f6a97174f876d3
aexiting <adamttway@gmail.com>
2019-05-03 01:00:26 -0400

    changing controller to do equal differently

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,8 @@
 class StaticController < ApplicationController
     def show
         @user = current_user
-        @comments = Comment.joins(:user).where("user_id == ?", @user.id)
-        @posts = Post.joins(:user).where("user_id == ?", @user.id)
+        @comments = Comment.joins(:user).where("user_id = ?", @user.id)
+        @posts = Post.joins(:user).where("user_id = ?", @user.id)
         render params[:page]
     end
 end
